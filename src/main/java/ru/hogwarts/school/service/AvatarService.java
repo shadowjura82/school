c57ea2bdf01d0b2774c14b62fa8a190b2f6b02f0
Yurii <shadowjura82@gmail.com>
2023-10-14 16:26:57 +0200

    Completed

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,28 +21,22 @@ import java.util.Collection;
 public class AvatarService {
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
+    private final Logger logger = LoggerFactory.getLogger(FacultyService.class);
     @Value("${path.to.avatars.folder}")
     private String filePath;
 
     public AvatarService(AvatarRepository avatarRepository, StudentRepository studentRepository) {
         this.avatarRepository = avatarRepository;
         this.studentRepository = studentRepository;
+        logger.debug("Constructor has been invoked successfully. AvatarService bin has been created in Spring context");
     }
 
     public void addAvatar(Long studentId, MultipartFile file) throws IOException {
+        logger.info("Method addAvatar has been invoked successfully");
         Student student = studentRepository.findById(studentId)
                 .orElseThrow(() -> new RuntimeException("Student with ID " + studentId + " hasn't been found in database"));
         Path path = Path.of(filePath, studentId + getExtension(file.getOriginalFilename()));
         if (Files.notExists(path.getParent())) Files.createDirectory(path.getParent());
-//        Files.deleteIfExists(path);
-//        try (
-//                InputStream inputStream = file.getInputStream();
-//                OutputStream outputStream = Files.newOutputStream(path, CREATE_NEW);
-//                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 1024);
-//                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 1024);
-//        ) {
-//            bufferedInputStream.transferTo(bufferedOutputStream);
-//        }
         file.transferTo(path);
         Avatar avatar = findAvatar(studentId);
         avatar.setStudent(student);
@@ -52,10 +48,12 @@ public class AvatarService {
     }
 
     private String getExtension(String fileName) {
+        logger.info("Method getExtension has been invoked successfully");
         return fileName.substring(fileName.lastIndexOf('.'));
     }
 
     private Avatar findAvatar(Long studentId) {
+        logger.info("Method findAvatar has been invoked successfully");
         Avatar avatar = avatarRepository.findByStudentId(studentId);
         if (avatar == null) {
             avatar = new Avatar();
@@ -64,10 +62,12 @@ public class AvatarService {
     }
 
     public Avatar getAvatar(Long studentId) {
+        logger.info("Method getAvatar has been invoked successfully");
         return avatarRepository.findByStudentId(studentId);
     }
 
     public Collection<Avatar> findAllWithPage(int page, int size) {
+        logger.info("Method findAllWithPage has been invoked successfully");
         PageRequest pageRequest = PageRequest.of(page - 1, size);
         Page<Avatar> pageAvatar = avatarRepository.findAll(pageRequest);
         return pageAvatar.getContent();
---
+++ src/main/java/ru/hogwarts/school/service/interfaces/InfoInterface.java
@@ -0,0 +1,5 @@
+package ru.hogwarts.school.service.interfaces;
+
+public interface InfoInterface {
+    String getPort();
+}
